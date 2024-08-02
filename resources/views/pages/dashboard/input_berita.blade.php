634ba2b091fe45c3b8fa7cd3e2234b49c5a56d8d
FirmanSaputra25 <you@example.com>
2024-08-03 00:22:32 +0700

    memperbaiki compenent

--- resources/views/pages/dashboard/input_berita.blade.php
+++ resources/views/pages/dashboard/input_berita.blade.php
@@ -1,4 +1,11 @@
 @extends('templates.layouts')
+
+@section('styles')
+<link rel="stylesheet" href="{{ asset('customs/adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('customs/adminlte/dist/css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ asset('customs/adminlte/plugins/summernote/summernote-bs4.min.css') }}">
+@endsection
+
 @section('sidebar')
 <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
@@ -834,4 +841,93 @@
     </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('customs/adminlte/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('customs/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('customs/adminlte/plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
+    <script src="{{ asset('customs/adminlte/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('customs/adminlte/plugins/summernote/summernote-bs4.min.js') }}"></script>
+
+    <!-- Page specific script -->
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            if (typeof $ !== 'undefined') {
+                $("#summernote").summernote();
+            } else {
+                console.error("jQuery is not loaded");
+            }
+            $('#exampleInputFile').change(function() {
+                let reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#previewImage').attr('src', e.target.result).show();
+                };
+                reader.readAsDataURL(this.files[0]);
+            });
+
+            $('#exampleInputFile').change(function() {
+                let reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#previewImage').attr('src', e.target.result).show();
+                    $('#removeImage').show();
+                };
+                reader.readAsDataURL(this.files[0]);
+
+                // Update the label with the file name
+                let fileName = this.files[0].name;
+                $(this).next('.custom-file-label').html(fileName);
+            });
+
+            $('#removeImage').click(function() {
+                $('#exampleInputFile').val(null);
+                $('#previewImage').hide();
+                $(this).hide();
+
+                // Reset custom file input label
+                $('.custom-file-label').html('Choose file');
+            });
+
+            $('#newsForm').on('submit', function(e) {
+                e.preventDefault(); // Mencegah form submit secara default
+
+                let formData = new FormData(this);
+
+                $.ajax({
+                    url: $(this).attr('action'),
+                    method: $(this).attr('method'),
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function(response) {
+                        // Berhasil
+                        alert('Berita berhasil ditambahkan!');
+
+                        // Reset form
+                        $('#newsForm')[0].reset();
+
+                        // Reset Summernote
+                        $('#summernote').summernote('reset');
+
+                        // Reset custom file input label
+                        $('#exampleInputFile').val(null);
+                        $('#previewImage').hide();
+                        $(this).hide();
+
+                        // Reset custom file input label
+                        $('.custom-file-label').html('Choose file');
+                    },
+                    error: function(xhr) {
+                        // Gagal
+                        let errors = xhr.responseJSON.errors;
+                        let errorMessage = 'Terjadi kesalahan:\n';
+                        $.each(errors, function(key, value) {
+                            errorMessage += value + '\n';
+                        });
+                        alert(errorMessage);
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
