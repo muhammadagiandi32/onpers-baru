444dec6002e683083778f6eef3ddc060f5e36327
unknown <muhammadagiandi@gmail.com>
2024-08-05 23:48:08 +0700

    memperbaiki open graph

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,9 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>onPers | Mulai Pemberitaan</title>
-        <!-- <meta name="description" content="{!! $data->content !!}"> -->
         <meta property="og:title" content="{{ $data->title }}">
-        <!-- <meta property="og:description" content="{!! $data->content !!}"> -->
         <meta property="og:image" content="{{ $data->image_url }}">
         <meta property="og:url" content="{{ url()->current() }}">
         <meta property="og:type" content="article">
