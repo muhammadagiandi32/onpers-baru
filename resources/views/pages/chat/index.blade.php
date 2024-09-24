b39712a40e646606920b634070527ee37da5e796
FirmanSaputra25 <you@example.com>
2024-09-24 21:53:32 +0700

    membuat compose untuk menulis pesan

--- resources/views/pages/chat/index.blade.php
+++ resources/views/pages/chat/index.blade.php
@@ -28,7 +28,7 @@
             <section class="content">
                 <div class="row">
                     <div class="col-md-3">
-
+                        <a href="{{ route('chat.fetch-messages') }}" class="btn btn-primary btn-block mb-3">Compose</a>
                         <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Folders</h3>
