ae48c33527da62cec285f6a659b8a7831902c99a
FirmanSaputra25 <you@example.com>
2024-10-07 01:22:09 +0700

    memperbaiki bug 3

--- resources/views/pages/dashboard/dashboards.blade.php
+++ resources/views/pages/dashboard/dashboards.blade.php
@@ -61,8 +61,8 @@
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
-                    <a href="{{ route('news-details', $item->slug) }}" class="small-box-footer">More info <i
-                            class="fas fa-arrow-circle-right"></i></a>
+                    {{-- <a href="{{ route('news-details', $item->slug) }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a> --}}
                 </div>
             </div>
 
@@ -81,8 +81,8 @@
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="{{ route('news-details', $item->slug) }}" class="small-box-footer">More info <i
-                            class="fas fa-arrow-circle-right"></i></a>
+                    {{-- <a href="{{ route('news-details', $item->slug) }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a> --}}
                 </div>
             </div>
         </div>
