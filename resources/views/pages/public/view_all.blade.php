f80aee2f867cc76234efbc146d45e8b18c7588ec
MacBook Pro <muhammadagiandi@gmail.com>
2025-03-15 21:41:41 +0700

    [update] fix bug iklan and berita upload

--- resources/views/pages/public/view_all.blade.php
+++ resources/views/pages/public/view_all.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,7 +24,6 @@
     <!-- Summernote CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote.min.css" rel="stylesheet">
     <style>
-        
         /* start css untuk nav dan header */
         body {
             background-color: #f4f4f4;
@@ -85,21 +83,24 @@
 
         @media (min-width: 768px) {
             .navbar-toggler {
-                display: none; /* Sembunyikan tombol burger di layar besar */
+                display: none;
+                /* Sembunyikan tombol burger di layar besar */
             }
 
             .b-search {
-                display: flex; /* Tampilkan search bar di layar besar */
+                display: flex;
+                /* Tampilkan search bar di layar besar */
             }
         }
-   
+
         /* end css untuk nav dan header */
 
 
-        html, body {
+        html,
+        body {
             height: 100%;
         }
-       
+
         .main-content {
             flex: 1;
         }
@@ -118,13 +119,13 @@
             color: white;
             padding: 20px 0;
             text-align: center;
-            margin-top: auto; /* Membuat footer berada di bawah */
+            margin-top: auto;
+            /* Membuat footer berada di bawah */
         }
 
         .container p {
             margin: 0;
         }
-
     </style>
 </head>
 
@@ -133,16 +134,18 @@
     <header class="header">
         <div class="container d-flex justify-content-between align-items-center">
             <a href="{{ url('/') }}" class="navbar-brand text-white font-weight-bold d-flex align-items-center">
-                <img src="{{ asset('img/logo/logo-onpers.png') }}" alt="Logo" style="height: 40px; margin-right: 8px;"> onPers
+                <img src="{{ asset('img/logo/logo-onpers.png') }}" alt="Logo"
+                    style="height: 40px; margin-right: 8px;"> onPers
             </a>
             @php
                 $currentCategory = request()->segment(2); // Mengambil segmen ke-2 dari URL, yaitu 'advertorial'
             @endphp
-            <form  action="{{ route('category.viewAll', $category) }}" method="GET" class="b-search d-none d-md-flex">
+            <form action="{{ route('category.viewAll', $category) }}" method="GET" class="b-search d-none d-md-flex">
                 <input type="text" class="form-control mr-2" placeholder="Search" name="search">
                 <button class="btn btn-light"><i class="fa fa-search"></i></button>
             </form>
-            <button class="navbar-toggler d-md-none" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+            <button class="navbar-toggler d-md-none" type="button" data-toggle="collapse"
+                data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
             </button>
         </div>
@@ -153,8 +156,10 @@
     <nav class="navbar navbar-expand-md navbar-custom">
         <div class="container">
             <div class="collapse navbar-collapse" id="navbarCollapse">
-                <form  action="{{ route('category.viewAll',  $currentCategory) }}" method="GET" class="b-search mb-2 d-md-none">
-                    <input type="text" class="form-control mb-2" placeholder="Search" name="search" value="{{ request('search') }}">
+                <form action="{{ route('category.viewAll', $currentCategory) }}" method="GET"
+                    class="b-search mb-2 d-md-none">
+                    <input type="text" class="form-control mb-2" placeholder="Search" name="search"
+                        value="{{ request('search') }}">
                     <button class="btn btn-light btn-block"><i class="fa fa-search"></i> Search</button>
                 </form>
                 <ul class="navbar-nav mr-auto">
@@ -174,7 +179,8 @@
                 @if (Auth::check())
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a href="{{ route('add-news') }}" class="nav-link"><i class="fa fa-upload"></i> Upload Berita</a>
+                            <a href="{{ route('add-news') }}" class="nav-link"><i class="fa fa-upload"></i> Upload
+                                Berita</a>
                         </li>
                     </ul>
                 @endif
@@ -185,27 +191,30 @@
 
     <!-- Main Content -->
     <div class="container main-content">
-    <h3 class="col-md-4 mb-3">Category: {{ ucfirst($category) }}</h3>
-    <div class="row">
-    @foreach($newsItems as $news)
-        <div class="col-md-4 mb-4">
-            <div class="card position-relative border-0 overflow-hidden" style="height: 200px; border-radius: 12px;">
-                <img src="{{ $news->image_url }}" class="h-100 w-100" alt="{{ $news->title }}" style="object-fit: cover; border-radius: 12px;">
-                <div class="card-img-overlay d-flex flex-column justify-content-end" style="background: rgba(0, 0, 0, 0.4); border-radius: 12px;">
-                <!-- 
+        <h3 class="col-md-4 mb-3">Category: {{ ucfirst($category) }}</h3>
+        <div class="row">
+            @foreach ($newsItems as $news)
+                <div class="col-md-4 mb-4">
+                    <div class="card position-relative border-0 overflow-hidden"
+                        style="height: 200px; border-radius: 12px;">
+                        <img src="{{ $news->image_url }}" class="h-100 w-100" alt="{{ $news->title }}"
+                            style="object-fit: cover; border-radius: 12px;">
+                        <div class="card-img-overlay d-flex flex-column justify-content-end"
+                            style="background: rgba(0, 0, 0, 0.4); border-radius: 12px;">
+                            <!--
                     kalo butuh buat nambahin category kaya badge
-                    <span class="badge bg-danger mb-2" style="width: fit-content;">Berita</span> 
+                    <span class="badge bg-danger mb-2" style="width: fit-content;">Berita</span>
                 -->
-                <a href="{{ route('news-details', $news->slug) }}">
-                    <h5 class="card-title text-white fw-bold">{{ Str::limit($news->title, 50) }}</h5>
-                </a>
+                            <a href="{{ route('news-details', $news->slug) }}">
+                                <h6 class="card-title text-white fw-bold">{{ $news->title, 50 }}</h6>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-    @endforeach
-</div>
 
-</div>
+    </div>
     <!-- Main Content End -->
 
     <!-- Footer Start -->
@@ -220,7 +229,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('customs/adminlte/plugins/summernote/summernote-bs4.min.js') }}"></script>
-   
+
 </body>
 
 </html>
